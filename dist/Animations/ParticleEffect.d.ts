0831535bce0c8999c31909d2b4e92e6660ee0496
Naibuu <hawkwer141099@gmail.com>
2024-05-12 22:32:29 +0300

    why is dist even in git?

--- dist/Animations/ParticleEffect.d.ts
+++
@@ -1,8 +0,0 @@
-import { IParticleEffect } from '../Schema/Animation';
-import { AnimationEffect } from './AnimationEffect';
-import { IDisposable } from '../Disposable';
-export declare class ParticleEffect extends AnimationEffect<IParticleEffect> {
-    protected disposables: IDisposable[];
-    tick(): void;
-    dispose(): void;
-}
--- dist/Animations/SoundEffect.d.ts
+++
@@ -1,5 +0,0 @@
-import { ISoundEffect } from '../Schema/Animation';
-import { AnimationEffect } from './AnimationEffect';
-export declare class SoundEffect extends AnimationEffect<ISoundEffect> {
-    tick(): void;
-}
